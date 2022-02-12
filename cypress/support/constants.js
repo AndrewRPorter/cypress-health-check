29d69e65b5d5ff46be124a233f95adbe622a4f9d
AndrewRPorter <pavelow53.andrew@gmail.com>
2022-02-12 16:48:54 -0500

    Add myqu website

--- cypress/support/constants.js
+++ cypress/support/constants.js
@@ -1,1 +1,5 @@
-export const SITES_TO_TEST = ["https://google.com", "https://clubtracker.golf"];
+export const SITES_TO_TEST = [
+  "https://google.com",
+  "https://clubtracker.golf",
+  "https://myqu.us",
+];
